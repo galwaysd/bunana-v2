9dc54a9f5e58fb0bbe52bc1919aa1b05b218b51e
galwaysd <j64r47sfmp@privaterelay.appleid.com>
2026-08-11 01:32:32 +0800

    style(topbar): center workbench and fabric market nav items

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -31,7 +31,7 @@ export default function TopBar() {
         </Link>
       </nav>
 
-      <div style={{ marginLeft: "auto" }}>
+      <div>
         <LanguageSwitcher />
       </div>
     </header>
